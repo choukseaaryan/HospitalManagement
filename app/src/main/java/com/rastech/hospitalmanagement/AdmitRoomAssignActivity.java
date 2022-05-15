bc6b76d9ece055b3486738aeefa50aa657032406
rs3523 <rohitsaini3523@gmail.com>
2022-05-15 14:42:05 +0530

    File1

---
+++ app/src/main/java/com/rastech/hospitalmanagement/AdmitRoomAssignActivity.java
@@ -0,0 +1,14 @@
+package com.rastech.hospitalmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdmitRoomAssignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admit_room_assign);
+    }
+}
