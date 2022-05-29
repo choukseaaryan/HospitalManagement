308ad082e04752d9c0bd4ffb6a2755933428efc8
rs3523 <rohitsaini3523@gmail.com>
2022-05-29 23:25:28 +0530

    Completed!
    Very Good 👍

--- app/src/main/java/com/rastech/hospitalmanagement/PatientPageActivity.java
+++ app/src/main/java/com/rastech/hospitalmanagement/PatientPageActivity.java
@@ -16,13 +16,21 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class PatientPageActivity extends AppCompatActivity {
-    Button logoutPatient;
+    Button logoutPatient,bookAppointPatient;
+    SQLiteDatabase db;
+    String Data ="";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_page);
+        db=openOrCreateDatabase("MyHospital", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS appointments(patientid integer);");
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        Data = str;
         logoutPatient = (Button)findViewById(R.id.logoutPatient);
-
+        bookAppointPatient=(Button)findViewById(R.id.bookAppointPatient);
         logoutPatient = findViewById(R.id.logoutPatient);
         logoutPatient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +39,23 @@ public class PatientPageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        bookAppointPatient = findViewById(R.id.bookAppointPatient);
+        bookAppointPatient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor c1=db.rawQuery("SELECT * FROM PatientData WHERE loginEmail='" +Data+"'" , null);
+                //String s = c1.getString(1);
+                db.execSQL("INSERT INTO appointments VALUES('" + str + "');");
+                showMessage("Status","Your Appointment is Set ");
+            }
+        });
+    }
+    public void showMessage(String title,String message)
+    {
+        Builder builder=new Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
